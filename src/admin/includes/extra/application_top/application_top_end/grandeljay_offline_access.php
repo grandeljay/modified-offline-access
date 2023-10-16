731c0a302b0d290147e9016b61de9d47711a27b7
grandeljay <github@grandels.email>
2023-10-16 11:35:05 +0200

    fix: fatal error during module edit

--- src/admin/includes/extra/application_top/application_top_end/grandeljay_offline_access.php
+++ src/admin/includes/extra/application_top/application_top_end/grandeljay_offline_access.php
@@ -12,6 +12,13 @@ if (!defined('MODULE_GRANDELJAY_OFFLINE_ACCESS_STATUS') || 'true' !== MODULE_GRA
     return;
 }
 
+if (
+       isset($_GET['module']) && 'grandeljay_offline_access' === $_GET['module']
+    && isset($_GET['action']) && 'edit'                      === $_GET['action']
+) {
+    return;
+}
+
 include_once DIR_FS_ADMIN . '/includes/modules/system/grandeljay_offline_access.php';
 
 grandeljay_offline_access::setCookieOfflineAccess();
